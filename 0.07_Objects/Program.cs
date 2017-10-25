1e5e4878bdd23f46215922e76d79d7c3f192381c
Kevin Curnow <kevinscurnow@gmail.com>
2017-10-25 16:52:08 -0400

    added objects and reverse

---
+++ 0.07_Objects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Donut PaulDonut = new Donut();
+            Donut KennDonut = new Donut();
+            Donut KevinDonut = new Donut();
+
+            KennDonut.Filling = "Strawberry Jelly";
+            KennDonut.Price = 0;
+            KennDonut.Type = "Jelly";
+            KennDonut.IsSpecial = false;
+
+            KevinDonut.Filling = "Chocolate PB";
+            KevinDonut.Price = 29.94M;
+            KevinDonut.Type = "Chócolate";
+            KevinDonut.IsSpecial = true;
+
+            PaulDonut.Filling = "Cherry Jelly";
+            PaulDonut.Price = 7.99M;
+            PaulDonut.Type = "Jelly";
+            PaulDonut.IsSpecial = true;
+
+            Console.WriteLine(PaulDonut);
+            Console.WriteLine(PaulDonut.Filling);
+            Console.WriteLine(KevinDonut.Type);
+            Console.ReadLine();
+        }
+    }
+}
