f3bd4f28120d45f9b3d662fd752634d3fe2c728a
Kevin Curnow <kevinscurnow@gmail.com>
2017-10-25 11:14:44 -0400

    Initial commit

---
+++ 0.02_BasicTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 255; // range: 0 to 255 size: 1 byte (one of 2 types, bool) use: seldom
+            int number1 = 15;
+            float myFloat = 3.14f; // need f
+            double myDouble = 2.54; // no explicit type change needed
+            decimal myDecimal = 3.14159m; // need some sort of m
+
+            char myLetter = 'A'; // remember to use single quotes for char
+            string myName = "Kevin"; // remember double quotes for string
+
+            bool myTruth = true;
+            
+
+            // Declaration
+            // <type> <variableName> <optional initializer>;
+            int age;
+            string country;
+
+            // Initialization
+            age = 41;
+            Console.WriteLine(age);
+            age = 42;
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 0.02_BasicTypesChallenges/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypesChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno;
+            int numeroDos = 2;
+            Console.WriteLine(numeroDos);
+
+            string aName;
+            string midName = "Scott";
+            Console.WriteLine(midName);
+
+            bool trueFalse;
+            bool falseTrue = false;
+            Console.WriteLine(falseTrue);
+
+            float floatOn;
+            float keepFloating = -2.994f;
+            Console.WriteLine(keepFloating);
+
+            double prep4Trouble;
+            double makeDouble = 2.468;
+            Console.WriteLine(makeDouble);
+
+            decimal deweyDec;
+            decimal deweyNum = 0.7734m;
+            Console.WriteLine(deweyNum);
+
+
+            string Combo = midName + " " + makeDouble;
+            Console.WriteLine(Combo);
+
+            string birthYear = "1994";
+            int yearInt = Convert.ToInt32(birthYear);
+            Console.WriteLine("I was born in " + yearInt + ".");
+            Console.ReadLine();
+        }
+    }
+}
