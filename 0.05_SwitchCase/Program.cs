0055b0fc884160bd28b4f1bdfbe85435073e3770
Kevin Curnow <kevinscurnow@gmail.com>
2017-10-25 14:26:19 -0400

    added switch case

---
+++ 0.05_SwitchCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._05_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Ken";
+
+            switch (friend)
+            {
+                case "Jay":
+                    Console.WriteLine("Hey Jay.");
+                    break;
+                case "Paul":
+                    Console.WriteLine("Sup Paul.");
+                    break;
+                case "Ken":
+                    Console.WriteLine("Go Dawgs!");
+                    break;
+                default:
+                    Console.WriteLine("I don't know you.");
+                    break;
+            }
+        }
+    }
+}
