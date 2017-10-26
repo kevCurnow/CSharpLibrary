ddaa061048060fb6956affd80b3afb34cb3951a1
Kevin Curnow <kevinscurnow@gmail.com>
2017-10-26 11:06:26 -0400

    added class challenges

---
+++ 0.08_ClassChallenges/Netflix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_ClassChallenges
+{
+    class Netflix
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        public string CheckRating()
+        {
+            if (this.Rating > 4)
+            {
+                return "This is a must watch!";
+            }
+            else if (this.Rating >=3 )
+            {
+                return "You might like this.";
+            }
+            else
+            {
+                return "Don't waste your time.";
+            }
+
+        }
+    }
+}
---
+++ 0.08_ClassChallenges/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_ClassChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Netflix show1 = new Netflix();
+            Netflix show2 = new Netflix();
+            Netflix show3 = new Netflix();
+
+            List<Netflix> shows = new List<Netflix>
+            {
+                show1,
+                show2,
+                show3
+            };
+
+
+            show1.Name = "Stranger Things";
+            show1.Genre = "Sci-Fi/Horror";
+            show1.Rating = 5;
+
+            show2.Name = "How I Met Your Mother";
+            show2.Genre = "Comedy";
+            show2.Rating = 4;
+
+            show3.Name = "Iron Fist";
+            show3.Genre = "Superhero";
+            show3.Rating = 1;
+
+            //Console.WriteLine(Show3.CheckRating());
+            foreach (Netflix element in shows)
+            {
+                Console.WriteLine(element.Name);
+                Console.WriteLine(element.Genre);
+                Console.WriteLine(element.Rating);
+                Console.WriteLine(element.CheckRating());
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
