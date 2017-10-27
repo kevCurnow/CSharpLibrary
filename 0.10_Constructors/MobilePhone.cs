1846d6d3dfabdfde1149329fc433c6278c61efbf
Kevin Curnow <kevinscurnow@gmail.com>
2017-10-27 10:59:38 -0400

    added constructors

---
+++ 0.10_Constructors/MobilePhone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_Constructors
+{
+    class MobilePhone
+    {
+        public MobilePhone(string friendName, string make, string model)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.FriendName = friendName;
+        }
+
+        public MobilePhone(string make, string model)
+        {
+            this.Make = make;
+            this.Model = model;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string FriendName { get; set; }
+        public string GetPhone()
+        {
+            return ($"{FriendName} has a/an {Make} {Model}.");
+        }
+    }
+}
---
+++ 0.10_Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student jayStudent = new Student("Jay", "Jackson", 5, 1);
+            Student paulStudent = new Student("Paul", "O'Connor", 12);
+            Student kevinStudent = new Student("Kevin");
+            Student joeStudent = new Student(23, "Joe", "Bir");
+            Console.WriteLine(jayStudent.LastName);
+
+            MobilePhone phone1 = new MobilePhone("Samsung", "Galaxy S8");
+            MobilePhone phoneBob = new MobilePhone("Bob", "iPhone", "8");
+            MobilePhone phoneJoe = new MobilePhone("Joe", "Samsung", "Galaxy S6");
+            MobilePhone phoneBill = new MobilePhone("Bill", "Nokia", "Smartphone");
+            MobilePhone phoneSteve = new MobilePhone("Steve", "Blackberry", "Pie");
+            MobilePhone phoneGeorge = new MobilePhone("George", "Motorola", "Razr");
+
+            List<MobilePhone> phones = new List<MobilePhone>() { phoneBob, phoneJoe, phoneBill, phoneSteve, phoneGeorge };
+          
+
+            foreach (MobilePhone phone in phones)
+            {
+                Console.WriteLine(phone.GetPhone());
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
